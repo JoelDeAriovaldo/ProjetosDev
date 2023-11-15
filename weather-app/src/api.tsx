e9563e7ab05847098817cea5e0dfe7f475e00b22
Joel De Ariovaldo Romao <joeldeariovaldodevef@gmail.com>
2023-11-15 07:49:26 +0000

    adicionandos icons ao projeto

---
+++ weather-app/src/api.tsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e58bd7267bmshe50fead54c962bcp1ee02fjsn56c3ed75cd9c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "fec27e803590843712884f708ba10023";
