25bcaac9d610fd8f4c83ea8749a27ad9a3e2e546
Joel De Ariovaldo Romao <joeldeariovaldodevef@gmail.com>
2023-11-16 12:17:07 +0000

    add sec valores

--- jobsearch-app/src/Components/JobDiv/Jobs.jsx
+++ jobsearch-app/src/Components/JobDiv/Jobs.jsx
@@ -1,30 +1,132 @@
-import React from 'react';
-import{ BiTimeFive } from 'react-icons/bi'
+import { BiTimeFive } from "react-icons/bi";
+import logo1 from "../../Assets/logo1.png";
+import logo2 from "../../Assets/logo2.png";
+import logo3 from "../../Assets/logo3.png";
+import logo4 from "../../Assets/logo4.png";
+import logo5 from "../../Assets/logo5.png";
+import logo6 from "../../Assets/logo6.png";
+import logo7 from "../../Assets/logo7.png";
+import logo8 from "../../Assets/logo8.png";
+
+const Data = [
+  {
+    id: 1,
+    image: logo1,
+    title: "Web Developer",
+    time: "Agora",
+    location: "Beira, Moçambique",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "Novac Linus Co.",
+  },
+  {
+    id: 2,
+    image: logo2,
+    title: "Designer graphic",
+    time: "Agora",
+    location: "Tete, Moçambique",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "Spotify AB",
+  },
+  {
+    id: 3,
+    image: logo3,
+    title: "Software engineer",
+    time: "Agora",
+    location: "California, USA",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "Netflix",
+  },
+  {
+    id: 4,
+    image: logo4,
+    title: "UI Designer",
+    time: "Agora",
+    location: "Maputo, Moçambique",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "TOYOTA",
+  },
+  {
+    id: 5,
+    image: logo5,
+    title: "Web Developer",
+    time: "Agora",
+    location: "London, UK",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "WhatsApp",
+  },
+  {
+    id: 6,
+    image: logo6,
+    title: "Network engineer",
+    time: "Agora",
+    location: "Porto, Portugual",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "YouTube",
+  },
+  {
+    id: 7,
+    image: logo7,
+    title: "UI/UX Designer",
+    time: "Agora",
+    location: "Moçambique",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "Telegram",
+  },
+  {
+    id: 8,
+    image: logo8,
+    title: "Product Manager",
+    time: "Agora",
+    location: "Moçambique",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+    company: "Twitter",
+  },
+];
 
 const Jobs = () => {
   return (
-    <div className="group group/item singleJob w-[250px] p-5 bg-white rounded
-    [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+    <div>
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+        {Data.map(({ id, image, title, time, location, desc, company }) => {
+          return (
+            <div key={id} className="group group/item singleJob w-[250px] p-5 bg-white rounded 10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg"
+            >
+              <span className="flex justify-between items-center gap-4">
+                <h1
+                  className="text-[16px] font-semibold text-textColor
+        group-hover:text-white"
+                >
+                 {title}
+                </h1>
+                <span className="flex items-center text-[#ccc] gap-1">
+                  <BiTimeFive />
+                  {time}
+                </span>
+              </span>
+              <h6 className="text-[#ccc] ">{location}</h6>
+              <p
+                className="text-[13px] text-[#959595] pt-[20px] border-t-[2px]
+      group-haver:text-white"
+              >
+                {desc}
+              </p>
 
-      <span className="flex justify-between items-center gap-4">
-        <h1 className="text-b font-semibold text-textColor
-        group-hover:text-white">Web Developer</h1>
-        <span className='flex items-center text-[#ccc] gap-1'>
-          <BiTimeFive/>Agora
-        </span>
-      </span>
-      <h6 className="text-[#ccc] ">Moçambique</h6> 
-      <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px]
-      group-haver:text-white'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga sunt quia exercitationem reprehenderit suscipit, neque dolor alias, culpa dolores earum voluptatem facilis et minus. Quas sit minima asperiores necessitatibus incidunt.
-      </p>
+              <div className="company flex items-center gap-2">
+                <img src={image} alt="Company logo" className="w-[10%]" />
+                <span className="text-[14px] py-[1rem] block group-hover:text-white">
+                  {company}
+                </span>
+              </div>
 
-      <div className="company flex items-center gap-2">
-        <img src="" alt="" />
+              <button className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-white">
+                Candidatar-se
+              </button>
+            </div>
+          );
+        })}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
--- jobsearch-app/src/Components/ValueDiv/Value.jsx
+++ jobsearch-app/src/Components/ValueDiv/Value.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import simple from '../../Assets/simple.png'
 
 const Value = () => {
   return (
-    <div>Value</div>
+    <div className="mb-[4rem] mt-[6rem]">
+      <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block">
+      O valor que nos mantém verdadeiros e responsáveis
+      </h1>
+
+      <div className="grid gap-[10rem] grid-cols-3 items-center">
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40]
+            w-[40px] flex items-center justify-center ">
+              <img src={simple} alt="" />
+
+              <span className='font-semibold text-textColor text-[18px]'>
+               Simplicidade
+              </span>
+              <div>
+                <p className="text-[25px] text-textColor opacity-[.7] py[1rem]">
+                Tornar as coisas bonitas e simples está no centro de tudo o que fazemos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
